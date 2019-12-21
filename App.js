ccf0c5eacd87f4812f219db8881e27df599c42a8
hazama16138 <like_apple0302@ezweb.ne.jp>
2019-12-21 14:48:15 +0900

    パッケージの追加

--- App.js
+++ App.js
@@ -1,19 +1,106 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Text, 
+  View, 
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import MapView from 'react-native-maps';
+import {
+  point,
+} from '@turf/helpers';
+import destination from '@turf/destination';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      elements: [],
+      south: null,
+      west: null,
+      north: null,
+      east: null,
+    }
+  }
+
+  onRegionChangeComplete = (region) => {
+    const center = point([region.longitude, region.latitude]);
+    const verticalMeter = 111 * region.latitudeDelta / 2;
+    const horizontalMeter = 111 * region.longitudeDelta / 2;
+    const options = {units: 'kilometers'};
+    const south = destination(center, verticalMeter, 180, options);
+    const west = destination(center, horizontalMeter, -90, options);
+    const north = destination(center, verticalMeter, 0, options);
+    const east = destination(center, horizontalMeter, 90, options);
+    this.setState({
+      south: south.geometry.coordinates[1],
+      west: south.geometry.coordinates[0],
+      north: south.geometry.coordinates[1],
+      east: south.geometry.coordinates[0],
+    });
+  };
+  fetchToilet = () => {
+
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView 
+          style={styles.mapview}
+          initialRegion={{
+            latitude: 35.681262,
+            longitude: 139.766403,
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00521,
+          }}
+        >
+        <MapView.Marker
+          coordinate={{
+            latitude: 35.681262,
+            longitude: 139.766403
+          }}
+          title="東京駅"
+        />
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.fetchToilet()}
+            style={styles.button}
+          >
+            <Text style={styles.buttonItem}>トイレ取得</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-end',
     backgroundColor: '#fff',
     alignItems: 'center',
+  },
+  mapview: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+  },
+  button: {
+    width: 150,
+    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  buttonItem: {
+    textAlign: 'center',
   },
 });
